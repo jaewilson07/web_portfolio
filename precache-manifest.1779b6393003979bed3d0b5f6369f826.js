deb92cedbd769f34a664785c86c3bd106ab5db06
Jae Wilson <jaewilson07@gmail.com>
2020-09-08 17:39:30 +0200

    Updates

---
+++ precache-manifest.1779b6393003979bed3d0b5f6369f826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a51d35b5542b9873ee0fba6b57540b",
+    "url": "/web_portfolio/index.html"
+  },
+  {
+    "revision": "bff5f1942f5cb2ea37ea",
+    "url": "/web_portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b50c2dcf8874b2624c98",
+    "url": "/web_portfolio/static/js/2.7316b301.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/web_portfolio/static/js/2.7316b301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff5f1942f5cb2ea37ea",
+    "url": "/web_portfolio/static/js/main.23526338.chunk.js"
+  },
+  {
+    "revision": "587b7fe482d164c86ca1",
+    "url": "/web_portfolio/static/js/runtime-main.226dd172.js"
+  }
+]);
